0543f4683db5086a354a507ebc39527b05fa374c
sharedlopezserrano <slopezserrano@byupathway.edu>
2026-01-24 02:50:14 -0300

    W02 Individual Activity: Dynamic Product List

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,21 +1,35 @@
-import { renderListWithTemplate } from './utils.mjs';
-import { productCardTemplate } from './main.js';
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="product_pages/?products=${product.Id}">
+        <img src="${product.Image}" alt="${product.Name}">
+        <h2>${product.Brand.Name}</h2>
+        <h3>${product.Name}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+    `;
+}
 
 export default class ProductList {
-    constructor(catagory, datasource, listElement) {
-        this.category = category
-        this.datasource = datasource;
-        this.listElement = listElement;
-    }
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
 
-    async init() {
-        // the dataSource will return a Promise...so you can use await to resolve it.
-        const list = await this.dataSpource.getData();
-        //next render the list
-        this.renderList(list);
-    }
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
 
-    renderList(list) {
-        renderListWithTemplate(productCardTemplate, this.listElement, list, 'afterbegin', true);
-    }
+  renderList(list) {
+        const htmlStrings = list.map(productCardTemplate);
+        this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+
+  }
+
 }
