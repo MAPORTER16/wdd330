76b944f9e709ef6db94eaa47fb153dc2a02a4188
sampromiseofficial <sampromiseofficial@gmail.com>
2026-01-28 02:32:39 +0100

    verified

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  base: "./",           // 🔥 REQUIRED for Render
+  envDir: "../",
 
   build: {
     outDir: "../dist",
+    emptyOutDir: true,  // 🔥 prevents broken old files
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
